f1963329ac2cdc571edc0ebccf8a17dbb77d7c15
Stack Harbour <som.ananad2005@gmail.com>
2026-02-15 19:05:28 +0530

    update

--- DSA/day8 - loop problems part1/q1.js
+++ DSA/day8 - loop problems part1/q1.js
@@ -1,2 +1,14 @@
 // checck if the number is prime or not 
 
+function isPrime(n) {
+    if (n<=1) return "Not Prime";
+
+    for(let i = 2; i<n ; i++){
+        if(n%i === 0) return "Not Prime";
+    }
+
+    return "Prime";
+    
+}
+
+module.exports = { isPrime };
