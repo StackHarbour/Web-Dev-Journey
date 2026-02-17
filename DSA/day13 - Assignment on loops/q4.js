c30aafc1aad4141c8e8f36327399fd5618abe640
Stack Harbour <som.ananad2005@gmail.com>
2026-02-17 19:12:24 +0530

    update

--- DSA/day13 - Assignment on loops/q4.js
+++ DSA/day13 - Assignment on loops/q4.js
@@ -1,2 +1,24 @@
 // check if a number is neon number 
 
+class Solution {
+    is_neon(n) {
+        
+        n = parseInt(n);
+        let sq = n**2;
+        let digit = 0;
+        let sqSum = 0;
+
+        if(n<0) return "No";
+
+        while(sq>0){
+            digit = sq%10;
+            sqSum += digit;
+            sq = Math.floor(sq/10);
+        }
+        
+        return sqSum === n ? "Yes" : "No";
+
+    }
+}
+
+module.exports = Solution;
